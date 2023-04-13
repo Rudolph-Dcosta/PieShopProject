ac285afa25adf3587242a7b9827d7a8e95eb9c34
RD69903 <RD69903@POOLW42SLPC7840>
2023-04-13 11:05:59 +0530

    Add project files.

---
+++ PieShop.Models/Pie.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PieShop.Models
+{
+    public class Pie
+    {
+        [Key]
+        public int PieId { get; set; }
+        [Required]
+        [Display(Name = "Pie Name")]
+        [RegularExpression(@"^[a-zA-Z]+[ a-zA-Z-]*$", ErrorMessage = "Pie name should contain only Characters")]
+        public string Name { get; set; }
+        [Display(Name = "Short Description")]
+        [Required]
+        [MinLength(30, ErrorMessage = "Short Description should have 30-50 Characters")]
+        [MaxLength(50, ErrorMessage = "Short Description should have 30-50 Characters")]
+        public string ShortDescription { get; set; }
+        [Display(Name = "Long Description")]
+        [Required]
+        public string LongDescription { get; set; }
+        [RegularExpression(@"^[-9]*$", ErrorMessage = "Price should be greater than 0")]
+        [Required]
+        public double Price { get; set; }
+        [ValidateNever]
+
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ PieShopProject/Controllers/PiesController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PieShop.DataAccess.Repository.IRepository;
+using PieShop.Models;
+using System.Drawing;
+
+namespace PieShopProject.Controllers
+{
+    public class PiesController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+
+        public PiesController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
+        }
+        public IActionResult Index()
+        {
+            var allPies = _unitOfWork.Pie.GetAll();
+            return View(allPies);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        //i have done some changes in below parameters
+        public IActionResult Create(Pie pieObj, IFormFile? file)
+        {
+
+            if (ModelState.IsValid)
+            {
+                string wwwRootPath = _webHostEnvironment.WebRootPath;
+                if (file != null)
+                {
+                    string fileName = file.FileName;
+                    var uploads = Path.Combine(wwwRootPath, @"images\pies");
+                    using (var fileStream = new FileStream(Path.Combine(uploads, fileName), FileMode.Create))
+                    {
+                         file.CopyToAsync(fileStream);
+                    }
+                    pieObj.ImageUrl = @"\images\pies\" + fileName;
+                    
+                }
+
+                _unitOfWork.Pie.Add(pieObj);
+                _unitOfWork.Save();
+                TempData["Success"] = "Pie added successfully";
+                return RedirectToAction("Index");
+            }
+            return View(pieObj);
+        }
+        public IActionResult Details(int pieId)
+        {
+            var pieDetails = _unitOfWork.Pie.GetFirstOrDefault(c => c.PieId == pieId);
+            return View(pieDetails);
+        }
+       
+    }
+}
